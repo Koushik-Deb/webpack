0f951a38f62afb2e7d3f28295efdc9791993d782
Koushik-007 <2015-516-802@student.du.ac.bd>
2024-02-19 22:53:44 +0600

    Add webpack config module asset/inline

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
                 test: /\.(png|jpg)$/,
                 type: 'asset/resource',
             },
+            {
+                test: /\.(svg)$/,
+                type: 'asset/inline',
+            }
         ],
     },
 };
