a7bc052730b6bd5dbe25178808fcf13f46e118e2
Koushik-007 <2015-516-802@student.du.ac.bd>
2024-02-23 00:00:59 +0600

    Custom options for code splitting

--- src/kiwi.js
+++ src/kiwi.js
@@ -1,9 +1,9 @@
 import Heading from "./components/heading/heading";
 import KiwiImage from "./components/kiwi-image/kiwi-image";
-import _ from 'lodash';
+import React from 'react'
 
 const heading = new Heading();
-heading.render(_.upperFirst('kiwi image'));
+heading.render('kiwi image');
 
 const kiwiImage = new KiwiImage();
 kiwiImage.render();
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -17,7 +17,7 @@ module.exports = {
   optimization: {
     splitChunks: {
       chunks: "all",
-
+      minSize: 3000,
     },
   },
   module: {
