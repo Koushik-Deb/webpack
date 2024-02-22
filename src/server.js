51a5b760315f5194025f81540efecdca1d5ae67c
Koushik-007 <2015-516-802@student.du.ac.bd>
2024-02-23 00:44:12 +0600

    Webpack with node and express in MPA done

--- src/server.js
+++ src/server.js
@@ -3,17 +3,23 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
+
+app.get('/hello-world', (req, res) => {
+    const pathToHtmlFile = path.resolve(__dirname, '../dist/hello-world.html');
+    const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
+    res.send(contentFromHtmlFile);
+});
+
+app.get('/kiwi', (req, res) => {
+    const pathToHtmlFile = path.resolve(__dirname, '../dist/kiwi.html');
+    const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
+    res.send(contentFromHtmlFile);
+});
+
 
 app.use('/static', express.static(path.resolve(__dirname, '../dist')));
 
-app.get('/', (req, res) => {
-    const pathToHtml = path.resolve(__dirname, '../dist/index.html');
-    const contentFromHtml = fs.readFileSync(pathToHtml, 'utf-8');
-
-    res.send(contentFromHtml);
-});
-
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`App is listening on ${PORT}`);
 });
