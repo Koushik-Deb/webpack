3e62c4a903e48d68cd07bdf9fcddaddef697fb43
Koushik Deb <koushik.deb@tigerit.com>
2024-02-22 13:33:30 +0600

    Manage different config for prod and dev

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ const heading = new Heading();
 const helloWorldButton = new HelloWorldButton();
 heading.render();
 helloWorldButton.render();
+
+if (process.env.NODE_ENV === "production") {
+  console.log("Production mode");
+} else if (process.env.NODE_ENV === "development") {
+  console.log("Development mode");
+} else {
+  console.log("None mode");
+}
