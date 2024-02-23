b40a35baadf02654dc6a80d55da61170f4e59272
Koushik-007 <2015-516-802@student.du.ac.bd>
2024-02-23 11:51:50 +0600

    Initial commit for module federation

---
+++ hello-world/src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = 9001;
+
+app.get('/', (req, res) => {
+    const pathToHtmlFile = path.resolve(__dirname, '../dist/hello-world.html');
+    const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
+    res.send(contentFromHtmlFile);
+});
+
+app.use('/static', express.static(path.resolve(__dirname, '../dist')));
+
+app.listen(PORT, () => {
+    console.log(`App is listening on ${PORT}`);
+});
