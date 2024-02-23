de215e72bae46709ca23dec044d4673f2b963b46
Koushik-007 <2015-516-802@student.du.ac.bd>
2024-02-23 21:12:32 +0600

    ESLINT integrated

--- src/components/heading/heading.js
+++ src/components/heading/heading.js
@@ -1,11 +1,11 @@
 import './heading.scss';
-import jQuery from 'jquery';
+
 class Heading {
     render(pageName) {
-        const h1 = jQuery('<h1>');
-        const body = jQuery('body');
-        h1.text('Webpack is awesome. This is "' + pageName + '" page');
-        body.append(h1);
+        const h1 = document.createElement('h1');
+        const body = document.querySelector('body');
+        h1.innerHTML = 'Webpack is awesome. This is "' + pageName + '" page';
+        body.appendChild(h1);
     }
 }
 
